6f489752174b4580c5e75d9d95da5371ed328a22
yeonnseok <ljinsk3@gmail.com>
2021-04-28 19:43:46 +0900

    add java activation library

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 	implementation("javax.xml.bind:jaxb-api:2.3.1")
 	implementation("com.sun.xml.bind:jaxb-core:2.3.0.1")
 	implementation("com.sun.xml.bind:jaxb-impl:2.3.3")
-	implementation("javax.activation:activation")
+	implementation("javax.activation:activation:1.1.1")
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	implementation("com.google.code.gson:gson:2.8.6")
 	implementation("com.google.guava:guava:30.1.1-jre")
