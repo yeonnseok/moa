bc49cf41d06688c287da5a4205dfc0bea6597345
yeonnseok <ljinsk3@gmail.com>
2021-04-28 16:41:05 +0900

    feat: naver 로그인 추가

--- src/main/kotlin/com/moa/auth/domain/OAuth2UserInfoFactory.kt
+++ src/main/kotlin/com/moa/auth/domain/OAuth2UserInfoFactory.kt
@@ -2,6 +2,7 @@ package com.moa.auth.domain
 
 import com.moa.auth.domain.dto.GoogleOAuth2UserInfo
 import com.moa.auth.domain.dto.KakaoOAuth2UserInfo
+import com.moa.auth.domain.dto.NaverOAuth2UserInfo
 import com.moa.auth.domain.dto.OAuth2UserInfo
 import com.moa.exceptions.OAuth2AuthenticationProcessingException
 import com.moa.user.domain.AuthProvider
@@ -21,6 +22,9 @@ class OAuth2UserInfoFactory
                 }
                 AuthProvider.KAKAO.equalWith(registrationId) -> {
                     KakaoOAuth2UserInfo(attributes)
+                }
+                AuthProvider.NAVER.equalWith(registrationId) -> {
+                    NaverOAuth2UserInfo(attributes)
                 }
                 else -> {
                     throw OAuth2AuthenticationProcessingException("$registrationId 로그인은 지원하지 않습니다.");
