433fe590e0b69749a68ac66a2fcd468564877c66
yeonnseok <ljinsk3@gmail.com>
2021-04-28 16:14:49 +0900

    feat: kakao 로그인 추가

---
+++ src/main/kotlin/com/moa/auth/domain/dto/KakaoOAuth2UserInfo.kt
@@ -0,0 +1,30 @@
+package com.moa.auth.domain.dto
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.moa.exceptions.OAuth2AuthenticationProcessingException
+
+
+class KakaoOAuth2UserInfo(
+    override val attributes: Map<String, Any>,
+) : OAuth2UserInfo(attributes) {
+
+    private val mapType = object : TypeToken<Map<String, Any>>() {}.type
+
+    override fun getId() = attributes.get("id").toString()
+
+    override fun getName(): String {
+        val properties: Map<String, Any> =
+            Gson().fromJson(attributes.get("properties").toString(), mapType)
+
+        return properties.get("nickname").toString()
+    }
+
+    override fun getEmail(): String {
+        val accounts: HashMap<String, Any> =
+            Gson().fromJson(attributes.get("kakao_account").toString(), mapType)
+
+        return accounts.get("email")?.toString()
+            ?: throw OAuth2AuthenticationProcessingException("이메일을 불러올 수 없습니다.")
+    }
+}
