a2285ac3ae3853c77cf3e008515b07fb0ef8ec22
yeonnseok <ljinsk3@gmail.com>
2021-04-28 19:39:34 +0900

    add jaxb library

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,10 @@ repositories {
 }
 
 dependencies {
+	implementation("javax.xml.bind:jaxb-api:2.3.1")
+	implementation("com.sun.xml.bind:jaxb-core:2.3.0.1")
+	implementation("com.sun.xml.bind:jaxb-impl:2.3.3")
+	implementation("javax.activation:activation")
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	implementation("com.google.code.gson:gson:2.8.6")
 	implementation("com.google.guava:guava:30.1.1-jre")
