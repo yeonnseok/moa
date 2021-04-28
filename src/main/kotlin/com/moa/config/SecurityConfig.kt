2e60f6b76919f52322794a2a75afa5978edbccfa
yeonnseok <ljinsk3@gmail.com>
2021-04-28 16:53:35 +0900

    refactor: security 설정

--- src/main/kotlin/com/moa/config/SecurityConfig.kt
+++ src/main/kotlin/com/moa/config/SecurityConfig.kt
@@ -67,7 +67,6 @@ class SecurityConfig(
             .authorizeRequests()
             .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
             .antMatchers("/api/v1/auth/**", "/login/oauth2/**").permitAll()
-            .antMatchers("/oauth/*").permitAll()
             .antMatchers("/").permitAll()
             .anyRequest().authenticated()
 
