78e750a8e01e4452da173307298d529bf1430a71
yeonnseok <ljinsk3@gmail.com>
2021-04-30 14:51:23 +0900

    fix: access docs

--- src/main/kotlin/com/moa/config/SecurityConfig.kt
+++ src/main/kotlin/com/moa/config/SecurityConfig.kt
@@ -36,14 +36,7 @@ class SecurityConfig(
 
     override fun configure(web: WebSecurity) {
         web.ignoring().antMatchers(
-            "/v2/api-docs",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**",
-            "/swagger/**"
+            "/docs/**"
         ).requestMatchers(PathRequest.toStaticResources().atCommonLocations())
     }
 
